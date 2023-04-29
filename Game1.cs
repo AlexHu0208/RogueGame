4c967f62302ea6dacfaa3141c6a06924172f2c8d
Alex Hu <ahu0208@gmail.com>
2023-04-29 17:26:35 -0400

    Init Game and Add Map

---
+++ Game1.cs
@@ -0,0 +1,136 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using RogueSharp;
+using RogueSharp.MapCreation;
+using RogueSharp.Random;
+
+namespace ExampleGame
+{
+    public class Game1 : Game
+    {
+        private GraphicsDeviceManager _graphics;
+        private SpriteBatch _spriteBatch;
+
+        private Texture2D _floor;
+        private Texture2D _wall;
+
+        private IMap _map;
+        private Player _player;
+
+        private InputState _inputState;
+
+        public Game1()
+        {
+            _graphics = new GraphicsDeviceManager(this);
+            Content.RootDirectory = "Content";
+            IsMouseVisible = true;
+        }
+
+        protected override void Initialize()
+        {
+            // TODO: Add your initialization logic here
+            IMapCreationStrategy<Map> mapCreationStrategy = new RandomRoomsMapCreationStrategy<Map>(50, 30, 100, 7, 3);
+            _map = Map.Create(mapCreationStrategy);
+            _inputState = new InputState();
+            base.Initialize();
+        }
+
+        protected override void LoadContent()
+        {
+            _spriteBatch = new SpriteBatch(GraphicsDevice);
+
+            // TODO: use this.Content to load your game content here
+            _floor = Content.Load<Texture2D>("Floor");
+            _wall = Content.Load<Texture2D>("Wall");
+            Cell startingCell = GetRandomEmptyCell();
+            _player = new Player
+            {
+                X = startingCell.X,
+                Y = startingCell.Y,
+                Scale = 0.25f,
+                Sprite = Content.Load<Texture2D>("Player")
+            };
+            UpdatePlayerFieldOfView();
+        }
+
+        protected override void Update(GameTime gameTime)
+        {
+            // TODO: Add your update logic here
+            _inputState.Update();
+            if (_inputState.IsExitGame(PlayerIndex.One))
+            {
+                Exit();
+            }
+            else
+            {
+                if (_player.HandleInput(_inputState, _map))
+                {
+                    UpdatePlayerFieldOfView();
+                }
+            }
+            base.Update(gameTime);
+        }
+
+        protected override void Draw(GameTime gameTime)
+        {
+            GraphicsDevice.Clear(Color.Black);
+
+            // TODO: Add your drawing code here
+            _spriteBatch.Begin(SpriteSortMode.BackToFront, BlendState.AlphaBlend);
+
+            int sizeOfSprites = 64;
+            float scale = .25f;
+            foreach(Cell cell in _map.GetAllCells())
+            {
+                var position = new Vector2(cell.X * sizeOfSprites * scale, cell.Y * sizeOfSprites * scale);
+                if (!cell.IsExplored)
+                {
+                    continue;
+                }
+                Color tint = Color.White;
+                if (!cell.IsInFov)
+                {
+                    tint = Color.Gray;
+                }
+                if (cell.IsWalkable)
+                {
+                    _spriteBatch.Draw(_floor, position, null, tint, 0.0f, new Vector2(0.0f, 0.0f), scale, SpriteEffects.None, 0.8f);
+                } else
+                {
+                    _spriteBatch.Draw(_wall, position, null, tint, 0.0f, new Vector2(0.0f, 0.0f), scale, SpriteEffects.None, 0.8f);
+                }
+            }
+
+            _player.Draw(_spriteBatch);
+
+            _spriteBatch.End();
+
+            base.Draw(gameTime);
+        }
+
+        private Cell GetRandomEmptyCell() {
+            IRandom random = new DotNetRandom();
+            while (true)
+            {
+                int x = random.Next(49);
+                int y = random.Next(29);
+                if (_map.IsWalkable(x, y))
+                {
+                    return (Cell)_map.GetCell(x, y);
+                }
+            }
+        }
+
+        private void UpdatePlayerFieldOfView()
+        {
+            _map.ComputeFov(_player.X, _player.Y, 30, true);
+            foreach(Cell cell in _map.GetAllCells())
+            {
+                if (_map.IsInFov(cell.X, cell.Y))
+                {
+                    _map.SetCellProperties(cell.X, cell.Y, cell.IsTransparent, cell.IsWalkable, true);
+                }
+            }
+        }
+    }
+}
